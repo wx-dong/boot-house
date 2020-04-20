05c2a214386d57a585335c48b184a67c031518a3
xd <1328532413@qq.com>
2020-04-20 11:46:35 +0800

    基本完成框架搭建

--- src/main/java/HouseApp.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by w on 2020/4/20.
- */
-public class HouseApp {
-}
---
+++ src/main/java/com/etoak/HouseApp.java
@@ -0,0 +1,14 @@
+package com.etoak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by w on 2020/4/20.
+ */
+@SpringBootApplication
+public class HouseApp {
+        public static void main(String[] args) {
+            SpringApplication.run(HouseApp.class, args);
+    }
+}
