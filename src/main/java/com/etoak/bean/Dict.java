d613803493ade0efde1b9798cd6530d686552be2
xd <1328532413@qq.com>
2020-04-20 14:58:56 +0800

    dev分支开发dict.java

---
+++ src/main/java/com/etoak/bean/Dict.java
@@ -0,0 +1,21 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+/**
+ * Created by w on 2020/4/20.
+ */
+@Data
+public class Dict {
+
+    private Integer id;
+
+    private String groupId;
+
+    private String name;
+
+    private String value;
+
+    private Integer sort;
+
+}
