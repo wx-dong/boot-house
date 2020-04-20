57f8b9842b33afa00fa4f28039738385436a9c26
xd <1328532413@qq.com>
2020-04-20 14:54:03 +0800

    dev分支开发user.java

---
+++ src/main/java/com/etoak/bean/User.java
@@ -0,0 +1,24 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+/**
+ * Created by w on 2020/4/20.
+ */
+@Data
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String gender;
+
+    private Integer age;
+
+    private String birthday;
+
+
+
+}
